46c2d665225fae817ec142b56eef24fd330b37fb
Nina Unger <ninunger@gmail.com>
2020-03-30 17:32:57 +0300

    finish room routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -3,25 +3,77 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Room = require("../models/room");
-
+// Get All rooms
 router.get("/", (req, res, next) => {
-    console.log("GET ROOM");
+    Room.find()
+    .then(rooms => res.json(rooms))
 });
-
+// Post new room
 router.post("/", (req, res, next) => {
-  console.log("POST ROOM")
+    const newRoom = new Room({
+        roomNum: req.body.roomNum,
+        floor: req.body.floor,
+        numOfpeople: req.body.numOfpeople,
+        roomPhone: req.body.roomPhone,
+        isAvailable: req.body.isAvailable
+    })
+    newRoom.save()
+    .then(room => res.json(room)); 
 });
-
-router.get("/:roomId", (req, res, next) => {
-    console.log("GET ROOM BY ID")
+//Get room
+router.get("/:roomId", getRoom,(req, res, next) => {
+    res.json(res.room);
 });
+// Get specific room
+function getRoom(req, res, next){
+    let id = req.params.roomId;
+    Room.findById(id)
+    .then(room => {
+        if (room){
+            res.room = room;
+        } else {
+            res.json({ message: 'Room not found' });
+            return;
+        }
+        next();
+    })
+}
 
+// Patch specific room
 router.patch("/:roomId", (req, res, next) => {
-    console.log("PATCH ROOM BY ID")
+    const id = req.params.roomId;
+   const updateOps = {};
+   for (const ops of req.body) {
+     updateOps[ops.propName] = ops.value;
+   }
+   Room.update({ _id: id }, { $set: updateOps })
+     .exec()
+     .then(result => {
+       console.log(result);
+       res.status(200).json(result);
+     })
+     .catch(err => {
+       console.log(err);
+       res.status(500).json({
+         error: err
+       });
+     });   
+});
+// Delete specific room
+router.delete("/:roomId", (req, res, next) => {
+   const id = req.params.roomId;
+   Room.remove({ _id: id })
+     .exec()
+     .then(result => {
+       res.status(200).json(result);
+     })
+     .catch(err => {
+       console.log(err);
+       res.status(500).json({
+         error: err
+       });
+     });
 });
 
-router.delete("/:roomId", (req, res, next) => {
-    console.log("DELETE ROOM BY ID")
-});
 
 module.exports = router;
